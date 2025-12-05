f159243726b1595a3d8843009b977ca38c54ae8d
syedturabb <turabhaiderpk@gmail.com>
2025-12-05 20:58:02 +0500

    First commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+           //create a file
+// fs.appendFile("hey.txt","ma to acha hu ", function(err){
+//       if(err) console.log(err);
+//       else console.log("done");
+      
+// })
+              //Rename a file
+// fs.rename("hey.txt","hello.txt",function(err){
+//    if(err) console.error(err);
+//    else console.log("Done");
+   
+   
+// })
+              //Copy File
+// fs.copyFile("hello.txt","./copy/copy.txt",function (err) {
+//         if(err)  console.error(err)
+//           else console.log("Done");
+          
+// })
+               //Removes file
+// fs.unlink("hello.txt", function(err){
+//   if(err) console.error(err)
+//     else console.log("Removed");
+    
+// })
+
+             //Removes folder
+// fs.rmdir("./copy",{recursive:true}, function(err){
+//   if(err) console.error(err)
+//     else console.log("removed");
+    
+// })
